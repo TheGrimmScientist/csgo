0a80f3196089ea870fc34286dbe3681f2a4413db
roboticminstrel <roboticminstrel@gmail.com>
2018-11-18 13:26:38 -0800

    initial page crawl on esea

--- crawl.py
+++ crawl.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+"""
+game IDs are consecutive, https://play.esea.net/match/142005420
+"""
+
 
 class CrawlSpider(scrapy.Spider):
     name = 'crawl'
-    allowed_domains = ['cs_spider']
-    start_urls = ['http://cs_spider/']
+    allowed_domains = ['play.esea.net']
+    start_urls = ['https://play.esea.net/match/12500000']
 
     def parse(self, response):
-        pass
+        print(response.text)
