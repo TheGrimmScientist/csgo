c050cab3c09a45e3091fc1c9385bb854b680e460
Allen Grimm <allen@grimmscience.com>
2019-07-14 13:56:20 -0700

    Finishing deletion of unused app.

--- csranker/settings.py
+++ csranker/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -39,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'esea_scraper',
-    'faceit_scaper',
 ]
 
 
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+sys.path.append(str(Path(__file__).parent / 'chromedriver'))
