36c42b9ed3ae23c7f8609d29f4e11e33fb88dccc
Allen Grimm <allen@grimmscience.com>
2019-08-18 12:27:17 -0700

    cleaned up test structure a bit.

--- esea_scraper/tests.py
+++ esea_scraper/tests.py
@@ -7,15 +7,23 @@ from csranker.settings import INVALID_GAME_PAGE
 from esea_scraper.management.commands.scrape_esea import identify_page_type
 
 
+def set_up_browser_for_testing():
+    browser = Browser('chrome', incognito=True, headless=False)
+    browser.driver.set_window_size(640, 480)
+    return browser
+
+
 class TestPageTypeChecker(TestCase):
+    def setUp(self):
+        self.browser = set_up_browser_for_testing()
+
     def test_invalid_match_properly_found(self):
         game_id = 14633573
 
-        browser = Browser('chrome', incognito=True, headless=False)
-        browser.driver.set_window_size(640, 480)
         url = 'https://play.esea.net/match/{}'.format(game_id)
-        browser.visit(url)
-
-        page_type = identify_page_type(browser)
+        self.browser.visit(url)
+        page_type = identify_page_type(self.browser)
 
         self.assertEqual(page_type, INVALID_GAME_PAGE)
+
+    
